13a7beb7eaff5ce44a5affb9fc65617367c7b422
Arash Tabatabaei <153722318+arashtabaa@users.noreply.github.com>
2025-10-15 22:41:19 +0330

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Wednesday 2025 React Native App</Text>
+    </View>
+  );
+}
