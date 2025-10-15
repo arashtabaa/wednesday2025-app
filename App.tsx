a6462fbb01a31e244fa7806ee91339cdf4a80c4a
Arash Tabatabaei <153722318+arashtabaa@users.noreply.github.com>
2025-10-15 22:42:56 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Wednesday 2025 React Native App</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Wednesday 2025 • React Native (Expo)</Text>
     </View>
   );
 }
